e2262db5d9f797ecc37bbf3e5bbb6d3d03183099
craycho <bakir.kreco@gmail.com>
2023-01-18 21:07:53 +0100

    Added smooth scrolling

--- src/js/controller.js
+++ src/js/controller.js
@@ -86,6 +86,10 @@ async function controlFilteredSpells(selectedSchools, selectedLevels) {
     model.state.selectedSchools = selectedSchools;
     model.state.selectedLevels = selectedLevels;
 
+    // Exit clause ako nema filtera
+    console.log(selectedLevels);
+    if (selectedSchools.length < 1 && selectedLevels.length < 1) return;
+
     // 1) Dopunjava query string na osnovu odabranih schools i levels
     let queryString = "?";
     selectedSchools.forEach(school => (queryString += `school=${school}&`));
--- src/js/views/spellView.js
+++ src/js/views/spellView.js
@@ -5,28 +5,39 @@ import searchView from "./searchView.js";
 
 class SpellView {
   displaySpell(spell) {
-    // 3) Displaya getani spell
+    // 1) Displaya getani spell
     searchView.suggestions.innerHTML = "";
     let spellLevelMarkup = "";
+    let higherLevel = "";
+    if (spell.higher_level[0] !== undefined)
+      higherLevel = "<b>At Higher Levels:</b> " + spell.higher_level[0];
 
     if (spell.level === 0) spellLevelMarkup = spell.school.name + " cantrip";
     else if (spell.level === 1)
-      spellLevelMarkup = "1st-level " + spell.school.index;
+      spellLevelMarkup = "1st-level " + spell.school?.index;
     else if (spell.level === 2)
-      spellLevelMarkup = "2nd-level " + spell.school.index;
+      spellLevelMarkup = "2nd-level " + spell.school?.index;
     else if (spell.level === 3)
-      spellLevelMarkup = "3rd-level " + spell.school.index;
-    else spellLevelMarkup = spell.level + "th-level " + spell.school.index;
+      spellLevelMarkup = "3rd-level " + spell.school?.index;
+    else spellLevelMarkup = spell.level + "th-level " + spell.school?.index;
 
     const spellMarkup = `<h3>${spell.name}</h3>
  <div class="type">${spellLevelMarkup}</div>
  <b>Casting Time:</b> ${spell.casting_time}<br>
  <b>Range:</b> ${spell.range}<br>
  <b>Components:</b>${spell.components.map(c => (c = " " + c))}<br>
- <b>Duration:</b> ${spell.duration}<br><br>
+ <b>Duration:</b> ${spell.duration}<span class="break-small"></span>
  ${spell.desc.map(element => `<p>${element}</p>`).join("")}
-
+ ${higherLevel}
 `;
+
+    // 2) Scrolla window do displayanog spella
+    window.scrollTo({
+      top: document.querySelector(".suggestions").getBoundingClientRect().top,
+      left: document.querySelector(".suggestions").getBoundingClientRect().left,
+      behavior: "smooth",
+    });
+
     document.querySelector(".spell-preview").style.display = "block";
     document.querySelector(".spell-preview").innerHTML = spellMarkup;
   }
